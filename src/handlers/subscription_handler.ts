b62520f47b47d3d3b6247400fdf7453d5a9a8816
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-10 20:54:49 +0700

    Fix lint and some other changes

--- src/handlers/subscription_handler.ts
+++ src/handlers/subscription_handler.ts
@@ -4,10 +4,13 @@ import { BaseHandler, Connection } from "./base_handler";
 export type SubscriptionCallback = (message: any) => void;
 
 export class SubscriptionHandler extends BaseHandler {
+  #callback: SubscriptionCallback;
 
-    #callback: SubscriptionCallback;
-
-  constructor(connection: Connection, id: string, callback: SubscriptionCallback) {
+  constructor(
+    connection: Connection,
+    id: string,
+    callback: SubscriptionCallback
+  ) {
     super(connection, id);
 
     this.#callback = callback;
@@ -17,8 +20,8 @@ export class SubscriptionHandler extends BaseHandler {
     super.handleMessage(message);
 
     if (message.type === MessageType.SUBSCRIPTION_MESSAGE) {
-      if (String(message.content['subscription_id']) === this.id) {
-        this.#callback(message.content.message);
+      if (String(message.content["subscription_id"]) === this.id) {
+        this.#callback(message.content["message"]);
       }
     }
   }
--- src/message.ts
+++ src/message.ts
@@ -2,31 +2,30 @@ export enum MessageType {
   CREATE_NODE,
   CREATE_SUBSCRIPTION,
   DESTROY_SUBSCRIPTION,
-  SUBSCRIPTION_MESSAGE
+  SUBSCRIPTION_MESSAGE,
 }
 
 export class Message {
-
-  static idCounter: number = 0;
+  static idCounter = 0;
 
   type: MessageType;
-  content: any
+  content: any;
   id: string;
 
   constructor(type: MessageType, content: any, id?: string) {
     this.type = type;
     this.content = content;
-    this.id = (id) ? id : String(Message.idCounter++);
+    this.id = id ? id : String(Message.idCounter++);
   }
 
   static parseMessage(message: string): Message {
     const jsonMessage = JSON.parse(message);
 
-    const typeKey = jsonMessage['type'] as keyof typeof MessageType;
+    const typeKey = jsonMessage["type"] as keyof typeof MessageType;
     const type = MessageType[typeKey];
 
-    const content = jsonMessage['content'];
-    const id = String(jsonMessage['id']);
+    const content = jsonMessage["content"];
+    const id = String(jsonMessage["id"]);
 
     return new Message(type, content, id);
   }
@@ -35,7 +34,7 @@ export class Message {
     return JSON.stringify({
       type: MessageType[this.type],
       id: this.id,
-      content: this.content
+      content: this.content,
     });
   }
 }

f370ccd2da3a02e2c9e5436aaa8fddbf304ade44
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-04-19 08:52:47 +0700

    Replace ECMASscript private with TypeScript private

--- src/bridge.ts
+++ src/bridge.ts
@@ -12,67 +12,67 @@ export type DisconnectCallback = (code: number, reason: string) => void;
 export type ErrorCallback = (err: Error) => void;
 
 export class Bridge {
-  #connection?: Connection;
-  #session?: SessionHandler;
+  private connection?: Connection;
+  private session?: SessionHandler;
 
-  #connectCallbacks: ConnectCallback[] = [];
-  #disconnectCallbacks: DisconnectCallback[] = [];
-  #errorCallbacks: ErrorCallback[] = [];
+  private connectCallbacks: ConnectCallback[] = [];
+  private disconnectCallbacks: DisconnectCallback[] = [];
+  private errorCallbacks: ErrorCallback[] = [];
 
   constructor() {
     return this;
   }
 
   onConnect(callback: ConnectCallback): Bridge {
-    this.#connectCallbacks.push(callback);
+    this.connectCallbacks.push(callback);
     return this;
   }
 
   onDisconnect(callback: DisconnectCallback): Bridge {
-    this.#disconnectCallbacks.push(callback);
+    this.disconnectCallbacks.push(callback);
     return this;
   }
 
   onError(callback: ErrorCallback): Bridge {
-    this.#errorCallbacks.push(callback);
+    this.errorCallbacks.push(callback);
     return this;
   }
 
   connect(url: string): Promise<SessionHandler> {
     return new Promise((resolve) => {
-      this.#connection = new Connection(url);
+      this.connection = new Connection(url);
 
-      this.#connection.onopen = (): void => {
-        if (!this.#connection) {
+      this.connection.onopen = (): void => {
+        if (!this.connection) {
           return;
         }
 
-        this.#session = new SessionHandler(this.#connection);
+        this.session = new SessionHandler(this.connection);
 
-        for (const callback of this.#connectCallbacks) {
-          callback(this.#session);
+        for (const callback of this.connectCallbacks) {
+          callback(this.session);
         }
 
-        resolve(this.#session);
+        resolve(this.session);
       };
 
-      this.#connection.onclose = (ev: ICloseEvent): void => {
-        if (this.#session) {
-          this.#session.cleanUp();
+      this.connection.onclose = (ev: ICloseEvent): void => {
+        if (this.session) {
+          this.session.cleanUp();
         }
 
-        for (const callback of this.#disconnectCallbacks) {
+        for (const callback of this.disconnectCallbacks) {
           callback(ev.code, ev.reason);
         }
       };
 
-      this.#connection.onmessage = (ev: IMessageEvent): void => {
+      this.connection.onmessage = (ev: IMessageEvent): void => {
         const message: Message = Message.parseMessage(String(ev.data));
-        this.#session?.handleMessage(message);
+        this.session?.handleMessage(message);
       };
 
-      this.#connection.onerror = (err: Error) => {
-        for (const callback of this.#errorCallbacks) {
+      this.connection.onerror = (err: Error) => {
+        for (const callback of this.errorCallbacks) {
           callback(err);
         }
       };
--- src/handlers/base_handler.ts
+++ src/handlers/base_handler.ts
@@ -19,36 +19,32 @@ export class BaseHandler {
   id: string;
   connection: Connection;
 
-  #active: boolean;
+  private active: boolean;
 
-  #parent: BaseHandler | null;
-  #childs: Map<string, BaseHandler>;
-
-  #executors: Map<MessageType, Map<string, Executor<Message>>>;
+  private childs: Map<string, BaseHandler>;
+  private executors: Map<MessageType, Map<string, Executor<Message>>>;
 
   constructor(connection: Connection, id: string) {
     this.connection = connection;
     this.id = id;
 
-    this.#active = true;
+    this.active = true;
 
-    this.#parent = null;
-    this.#childs = new Map();
-
-    this.#executors = new Map();
+    this.childs = new Map();
+    this.executors = new Map();
   }
 
   cleanUp(): boolean {
-    if (this.#active) {
-      this.#active = false;
+    if (this.active) {
+      this.active = false;
 
-      for (const map of this.#executors.values()) {
+      for (const map of this.executors.values()) {
         for (const executor of map.values()) {
           executor.reject(new Error("Handler destroyed"));
         }
       }
 
-      for (const child of this.#childs.values()) {
+      for (const child of this.childs.values()) {
         child.cleanUp();
       }
 
@@ -59,31 +55,28 @@ export class BaseHandler {
   }
 
   attach(child: BaseHandler): void {
-    child.#parent = this;
-    this.#childs.set(child.id, child);
+    this.childs.set(child.id, child);
 
-    if (!this.#active) {
+    if (!this.active) {
       child.cleanUp();
     }
   }
 
   detach(id: string): void {
-    if (this.#childs.has(id)) {
-      const child = this.#childs.get(id);
-      this.#childs.delete(id);
+    if (this.childs.has(id)) {
+      const child = this.childs.get(id);
+      this.childs.delete(id);
 
       if (child) {
-        if (child.#active) {
+        if (child.active) {
           child.cleanUp();
         }
-
-        child.#parent = null;
       }
     }
   }
 
   handleMessage(message: Message): void {
-    const map = this.#executors.get(message.type);
+    const map = this.executors.get(message.type);
     if (map) {
       const executor = map.get(message.id);
       if (executor) {
@@ -95,8 +88,8 @@ export class BaseHandler {
       }
     }
 
-    for (const child of this.#childs.values()) {
-      if (child.#active) {
+    for (const child of this.childs.values()) {
+      if (child.active) {
         child.handleMessage(message);
       }
     }
@@ -115,10 +108,10 @@ export class BaseHandler {
 
         this.send(message);
 
-        let map = this.#executors.get(message.type);
+        let map = this.executors.get(message.type);
         if (!map) {
           map = new Map();
-          this.#executors.set(message.type, map);
+          this.executors.set(message.type, map);
         }
 
         map.set(message.id, new Executor(resolve, reject));
